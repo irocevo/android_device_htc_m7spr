6565004fd53bf424df1c3b63aeae2b00f2aa06c2
irocevo <irocevo@users.noreply.github.com>
2016-01-18 13:17:06 -0500

    Update and rename cm.mk to crdroid.mk

--- crdroid.mk
+++ crdroid.mk
@@ -1,6 +1,6 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/crdroid/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/htc/m7spr/full_m7spr.mk)
 
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="htc/sprint_wwe/m7wls:5.0.2/LRX22F/461956.2:user/release-keys" \
     PRIVATE_BUILD_DESC="6.16.651.2 CL461956 release-keys"
 
-PRODUCT_NAME := cm_m7spr
+PRODUCT_NAME := crdroid_m7spr
 PRODUCT_DEVICE := m7spr
